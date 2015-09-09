1b3e1b19362d272b8a4129b3df9803ad93bd234f
maxmcd <max.t.mcdonnell@gmail.com>
2015-09-08 21:28:04 -0400

    working http streaming with Go, needed content type (DUH)

--- main.go
+++ main.go
@@ -1,37 +1,25 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 	"time"
 )
 
-type flushWriter struct {
-	f http.Flusher
-	w io.Writer
-}
+func handle(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	w.WriteHeader(200)
 
-func (fw *flushWriter) Write(p []byte) (n int, err error) {
-	n, err = fw.w.Write(p)
-	if fw.f != nil {
-		fw.f.Flush()
-	}
-	return
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
 	for {
-		fw := flushWriter{w: w}
-		if f, ok := w.(http.Flusher); ok {
-			fw.f = f
-		}
-		time.Sleep(time.Millisecond * 10000)
-		fw.Write([]byte("WHEEE\n"))
+		w.Write([]byte("young\r\n"))
+		w.(http.Flusher).Flush()
+		time.Sleep(time.Millisecond * 100)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+
+	http.HandleFunc("/", handle)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
